814fc6dad28a0d9cb898009e610c81d2e90e973a
Sushanta Kumar Pradhan <sushantapradhankumar67@gmail.com>
2025-06-05 20:09:15 +0530

    Delete php-app directory

--- php-app/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Destroy all session data
-setcookie("username", "", time() - 3600, "/"); // Expire the cookie
-header("Location: login.php"); // Redirect to login page
-exit();
-?>
